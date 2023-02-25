numerical bug fixed.

classical_ib.py
@@ -62,6 +62,7 @@ def classical_original_ib(args, pX, p_Y_X):
 
     previous_loss = float('inf')
     converged = False
+    converge_count = 0
     n = 0
     while not converged and n < 3000:
         previous_q_T_X = q_T_X.clone()
@@ -71,15 +72,22 @@ def classical_original_ib(args, pX, p_Y_X):
         q_Y_T = pXY @ q_T_X.T / q_T
         loss = torch.dist(previous_q_T_X, q_T_X)
         # print('loss:', loss.item())
+        args.writer.add_scalar("obj/J_fixed_gamma", loss.real, n)
         if abs(loss - previous_loss) < args.cvg_thres:
+            converge_count += 1
+        else:
+            converge_count = 0
+        if converge_count == 10:
             converged = True
         previous_loss = loss
         n += 1
     # print(q_T_X[:, 0])
-    q_TX = q_T_X * pX[None, :]
-    I_TX = (q_TX * (torch.log2(q_TX + 1e-12) - torch.log2(pX[None, :] * q_T[:, None]))).sum()
-    q_YT = q_Y_T * q_T[None, :]
-    I_YT = (q_YT * (torch.log2(q_YT + 1e-12) - torch.log2(q_T[None, :] * pY[:, None]))).sum()
+        q_TX = q_T_X * pX[None, :]
+        I_TX = (q_TX * (torch.log2(q_TX + 1e-12) - torch.log2(pX[None, :] * q_T[:, None]))).sum()
+        q_YT = q_Y_T * q_T[None, :]
+        I_YT = (q_YT * (torch.log2(q_YT + 1e-12) - torch.log2(q_T[None, :] * pY[:, None]))).sum()
+        f_alpha = I_TX - args.beta * I_YT
+        args.writer.add_scalar("obj/f_alpha", f_alpha, n)
     # print(f'Algorithm converged in {n} iterations.')
     return I_TX, I_YT, n
 
@@ -105,14 +113,18 @@ def main(args):
     y_coords = []
     betas = []
     iterations = []
-    for beta in tqdm(torch.arange(0.001, 50, 0.1)):
-        args.beta = beta
-        betas.append(beta)
-        I_TX, I_YT, n = classical_original_ib(args, pX, p_Y_X)
-        iterations.append(n)
-        x_coords.append(I_TX.cpu())
-        y_coords.append(I_YT.cpu())
+    # for beta in tqdm(torch.arange(0.001, 50, 0.1)):
+    #     args.beta = beta
+    #     betas.append(beta)
+    #     I_TX, I_YT, n = classical_original_ib(args, pX, p_Y_X)
+    #     iterations.append(n)
+    #     x_coords.append(I_TX.cpu())
+    #     y_coords.append(I_YT.cpu())
 
+    args.beta = 6.
+    I_TX, I_YT, n = classical_original_ib(args, pX, p_Y_X)
+
+    assert False
     fig = plt.figure()
     plt.scatter(x_coords, y_coords)
     plt.xlabel('I(X;T)')
@@ -129,21 +141,21 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--gamma', default=0.8, type=float)
+    parser.add_argument('--gamma', default=0.2, type=float)
     parser.add_argument('--alpha', default=1.0, type=float)
     parser.add_argument('--beta', default=20., type=float)
     parser.add_argument('--dim_X', default=2**8, type=int, help='dimension of X')
-    parser.add_argument('--dim_Y', default=2**5, type=int, help='dimension of Y')
+    parser.add_argument('--dim_Y', default=2**6, type=int, help='dimension of Y')
     parser.add_argument('--dim_T', default=2**8, type=int, help='dimension of T')
-    parser.add_argument('--cvg_thres', default=5e-4, type=int, help='threshold for convergence of algorithm')
+    parser.add_argument('--cvg_thres', default=1e-3, type=int, help='threshold for convergence of algorithm')
     args = parser.parse_args()
 
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    device = torch.device('cpu' if torch.cuda.is_available() else 'cpu')
     print(f'==== Running classical algorithm on {device}....')
 
     # set_seed(0)
-    # args.writer = SummaryWriter(log_dir='./logs')
+    args.writer = SummaryWriter(log_dir='./logs/classical-gamma-0.2-8')
     main(args)
-    # args.writer.flush()
-    # args.writer.close()
+    args.writer.flush()
+    args.writer.close()
     

quantum_qib.py
@@ -1,4 +1,6 @@
 import argparse
+import os
+import pickle
 from functools import reduce
 
 import numpy as np
@@ -36,6 +38,10 @@ def set_seed(seed):
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.use_deterministic_algorithms(True)
+
+
+def nan2num(a):
+    return torch.nan_to_num(a.real) + 1j * torch.nan_to_num(a.imag)
 
 
 def is_hermitian(a, tol=1e-2):
@@ -90,8 +96,11 @@ def matrix_log2(matrix):
     # assert is_hermitian(matrix), f'LOG: matrix {matrix} is not hermitian.'
     L, Q = torch.linalg.eigh(matrix)
     # assert (L.real > 0).all(), f'Non-positive values found in log. {L}'
-    eigvals = torch.where(L.real > 0, L.real, 1e-9)
-    return Q @ torch.diag_embed(torch.log2(eigvals)).cfloat() @ Q.mH
+    # eigvals = torch.where(L.real > 0, L.real, 1e-200)
+    eigvals = torch.diag_embed(torch.log2(L + 1e-12))
+    # eigvals = torch.nan_to_num(eigvals, nan=1e-20, neginf=1e-20)
+    a = Q @ eigvals.cdouble() @ Q.mH
+    return a
     # if len(matrix.shape) == 2:
     #     return torch.from_numpy(scipy.linalg.logm(matrix.cpu().numpy(), disp=False)[0]).to(matrix.device)
 
@@ -102,11 +111,16 @@ def matrix_log2(matrix):
 
 
 def matrix_exp(matrix):
-    assert is_hermitian(matrix), f'EXP: matrix {matrix} is not hermitian.'
+    # assert is_hermitian(matrix), f'EXP: matrix {matrix} is not hermitian.'
+    # print(matrix.real.max())
+    # matrix = nan2num(matrix)
     L, Q = torch.linalg.eigh(matrix)
-    eigens = torch.diag_embed(torch.exp(L))
+    mat = torch.diag_embed(torch.softmax(L, 1))
     # eigens = torch.nan_to_num(eigens.clamp(max=1e+9))
-    return Q @ eigens.cfloat() @ Q.mH
+    mat = Q @ mat.cdouble() @ Q.mH
+    return mat
+    # return torch.where(torch.isinf(mat), torch.full_like(mat, 1e+10), mat)
+    # return torch.nan_to_num(mat.real) + 1j * torch.nan_to_num(mat.imag)
 
 
 def rho_Y(pX, rho_Y_x):
@@ -171,7 +185,6 @@ def F_alpha(alpha, beta, pX, sigma_T_X, rho_Y_X):
     rho_y = rho_Y(pX, rho_Y_X)
     tmp_rho = batch_kron(eyes, rho_Y_X) @ (matrix_log2(batch_kron(sigma_t_TX, rho_y)) - \
                 matrix_log2(sigma_YT(pX, sigma_T_X, rho_Y_X)))
-
     return -matrix_log2(sigma_t_TX) + alpha * matrix_log2(sigma_T_X) + \
             beta * partial_trace(tmp_rho, 1, dims=[sigma_T_X.shape[1], rho_Y_X.shape[1]])
 
@@ -194,48 +207,56 @@ def f_gamma(alpha, beta, pX, sigma_T_X1, sigma_T_X2, rho_Y_X):
 
 
 def initialize_sigma_T_X(args):
-    # sigma_T_X = torch.stack([torch.from_numpy(random_density_matrix(args.dim_T).data).cfloat() for _ in range(args.dim_X)])
-    # sigma_T_X = torch.stack([torch.diag(torch.softmax(torch.rand(args.dim_T), -1)).cfloat()
+    sigma_T_X = [torch.from_numpy(random_density_matrix(args.dim_T).data).cdouble() for _ in range(args.dim_X)]
+    # sigma_T_X = torch.stack([torch.diag(torch.softmax(torch.rand(args.dim_T), -1)).cdouble()
     #                          for _ in range(args.dim_X)]).to(device)
-    sigma_T_X = []
-    for i in range(args.dim_X):
-        rand_vec = torch.rand(args.dim_T)
-        rand_vec[i] = 0
-        rand_vec = rand_vec / rand_vec.sum() * 0.25
-        rand_vec[i] = 0.75
-        sigma_T_X.append(torch.diag(rand_vec).cfloat())
+    # sigma_T_X = []
+    # for i in range(args.dim_X):
+    #     rand_vec = torch.rand(args.dim_T)
+    #     rand_vec[i] = 0
+    #     rand_vec = rand_vec / rand_vec.sum() * 0.25
+    #     rand_vec[i] = 0.75
+    #     sigma_T_X.append(torch.diag(rand_vec).cdouble())
     return torch.stack(sigma_T_X).to(device)
 
 
-def fixed_gamma_qib(args, pX, rho_Y_X, rho_X):
-    sigma_T_X = initialize_sigma_T_X(args)
+def fixed_gamma_qib(args, pX, rho_Y_X, rho_X, init_sigma_T_X):
+    sigma_T_X = init_sigma_T_X
     assert is_hermitian(sigma_T_X), 'sigma_T|X is not hermitian.'
     assert is_positive(sigma_T_X), f'sigma_T|X is not positive.'
     previous_loss = float('inf')
     converged = False
     n = 0
-    while not converged:
+    converge_count = 0
+    while not converged and n < 100:
         sigma_T_X0 = sigma_T_X.clone()
-        sigma_gamma_alpha_T = torch.matrix_exp(matrix_log2(sigma_T_X) - \
+        sigma_gamma_alpha_T = matrix_exp(matrix_log2(sigma_T_X) - \
                                 1 / args.gamma * F_alpha(args.alpha, args.beta, pX, sigma_T_X, rho_Y_X))
-        sigma_T_X = sigma_gamma_alpha_T / trace(sigma_gamma_alpha_T)[:, None, None]
+        sigma_T_X = sigma_gamma_alpha_T # / trace(sigma_gamma_alpha_T)[:, None, None]
         # assert is_hermitian(sigma_T_X), f'sigma_T|X is not hermitian. dist: {torch.dist(sigma_T_X, sigma_T_X.mH)}'
+        gamma_star = f_gamma(args.alpha, args.beta, pX, sigma_T_X0, sigma_T_X, rho_Y_X)
+
         if not is_hermitian(sigma_T_X):
-            return None, None, None
+            assert False
         loss = J_gamma_alpha(args.gamma, args.alpha, args.beta, pX, sigma_T_X, sigma_T_X0, rho_Y_X)
         # print('loss:', loss)
         f_alpha_value = f_alpha(args.alpha, args.beta, pX, sigma_T_X, rho_Y_X)
-        # args.writer.add_scalar('f_alpha', f_alpha_value.real, n)
-        # args.writer.add_scalar("J(a, a')", loss.real, n)
+        args.writer.add_scalar('obj/f_alpha_fixed_gamma', f_alpha_value.real, n)
+        args.writer.add_scalar("loss/J(a, a')_fixed_gamma", loss.real, n)
         # print('f_alpha:', f_alpha_value.item())
+        args.writer.add_scalar("gammas/gamma_star_fixed", gamma_star.real, n)
         if abs(loss.real - previous_loss.real) < args.cvg_thres:
+            converge_count += 1
+        else:
+            converge_count = 0
+        if converge_count == 10:
             converged = True
         previous_loss = loss
         n += 1
     # print(f'Algorithm converged in {n} iterations.')
 
     rho_t = sigma_T(pX, sigma_T_X)
-    rho_x = torch.diag(pX).cfloat()
+    rho_x = torch.diag(pX).cdouble()
     rho_xt = (pX[:, None, None] * batch_kron(rho_X, sigma_T_X)).sum(0)
     I_TX = entropy(rho_x) + entropy(rho_t) - entropy(rho_xt)
 
@@ -246,20 +267,22 @@ def fixed_gamma_qib(args, pX, rho_Y_X, rho_X):
     return I_TX, I_YT, n
 
 
-def adaptive_qib(args, pX, rho_Y_X, rho_X):
+def adaptive_qib(args, pX, rho_Y_X, rho_X, init_sigma_T_X):
     c = 1.0
     args.gamma = args.alpha
     sigma_T_X = Queue(max_len=4)
-    sigma_T_X.push(initialize_sigma_T_X(args))
+    sigma_T_X.push(init_sigma_T_X)
     sigma_gamma_alpha_T = matrix_exp(matrix_log2(sigma_T_X.getitem(-1)) - \
                           1 / args.gamma * F_alpha(args.alpha, args.beta, pX, sigma_T_X.getitem(-1), rho_Y_X))
-    sigma_T_X.push(sigma_gamma_alpha_T / trace(sigma_gamma_alpha_T)[:, None, None])
+    # sigma_T_X.push(sigma_gamma_alpha_T / trace(sigma_gamma_alpha_T)[:, None, None])
+    sigma_T_X.push(sigma_gamma_alpha_T)
     n = 2
     previous_loss = float('inf')
     converged = False
     lambda_n = 0.
 
-    while not converged:
+    converge_count = 0
+    while not converged and n < 100:
         gamma_star = f_gamma(args.alpha, args.beta, pX, sigma_T_X.getitem(1), sigma_T_X.getitem(2), rho_Y_X)
         if gamma_star.real > 0:
             if n == 2:
@@ -272,24 +295,38 @@ def adaptive_qib(args, pX, rho_Y_X, rho_X):
                            kl_distance(pX, sigma_T_X.getitem(3), sigma_T_X.getitem(2)) + \
                            kl_distance(pX, sigma_T_X.getitem(2), sigma_T_X.getitem(1))
             lambda_n = lambda_n.real
-            args.gamma = min(args.alpha, lambda_n * (args.alpha - gamma_star.real) + gamma_star.real)
-
+            # lambda_n = gamma_star.real
+            gamma = min(args.alpha, lambda_n * (args.alpha - gamma_star.real) + gamma_star.real)
+            if gamma > 0: args.gamma = gamma
+        # print(matrix_exp(matrix_log2(sigma_T_X.getitem(-1))).real.max())
+        # print(args.gamma)
+        # print((1 / args.gamma * F_alpha(args.alpha, args.beta, pX, sigma_T_X.getitem(-1), rho_Y_X).real.max()))
         sigma_gamma_alpha_T = matrix_exp(matrix_log2(sigma_T_X.getitem(-1)) - \
                                 1 / args.gamma * F_alpha(args.alpha, args.beta, pX, sigma_T_X.getitem(-1), rho_Y_X))
 
-        new_sigma_T_X = sigma_gamma_alpha_T / trace(sigma_gamma_alpha_T)[:, None, None]
+        new_sigma_T_X = sigma_gamma_alpha_T # / trace(sigma_gamma_alpha_T)[:, None, None]
         # assert is_hermitian(new_sigma_T_X), f'sigma_T|X is not hermitian. dist: {torch.dist(new_sigma_T_X, new_sigma_T_X.mH)}'
         if not is_hermitian(new_sigma_T_X):
+            assert False
             break
         sigma_T_X.push(new_sigma_T_X)
         loss = J_gamma_alpha(args.gamma, args.alpha, args.beta, pX, sigma_T_X.getitem(-1), sigma_T_X.getitem(-2), rho_Y_X)
+        f_alpha_value = f_alpha(args.alpha, args.beta, pX, sigma_T_X.getitem(-1), rho_Y_X)
+        args.writer.add_scalar("obj/f_alpha", f_alpha_value.real, n)
+        args.writer.add_scalar("loss/J(a, a')", loss.real, n)
+        args.writer.add_scalar("gammas/gamma", args.gamma, n)
+        # args.writer.add_scalar('f_alpha', f_alpha_value.real, n)
         if abs(loss.real - previous_loss.real) < args.cvg_thres:
+            converge_count += 1
+        else:
+            converge_count = 0
+        if converge_count == 10:
             converged = True
         previous_loss = loss
         n += 1
 
     rho_t = sigma_T(pX, sigma_T_X.getitem(-1))
-    rho_x = torch.diag(pX).cfloat()
+    rho_x = torch.diag(pX).cdouble()
     rho_xt = (pX[:, None, None] * batch_kron(rho_X, sigma_T_X.getitem(-1))).sum(0)
     I_TX = entropy(rho_x) + entropy(rho_t) - entropy(rho_xt)
 
@@ -305,11 +342,11 @@ def rho_theta_lambda(th, la):
         """assert A^2 = I"""
         theta = theta[:, None, None]
         return torch.cos(theta) * torch.eye(A.shape[1])[None] + 1j * torch.sin(theta) * A[None]
-    pauli_x = torch.tensor([[0, 1], [1, 0]], dtype=torch.cfloat, device=device)
+    pauli_x = torch.tensor([[0, 1], [1, 0]], dtype=torch.cdouble, device=device)
     pauli_xs = reduce(torch.kron, [pauli_x for _ in range(5)])
     # tmp = 1j * th[:, None, None] * pauli_x[None]
-    # return matrix_exp(tmp) @ torch.diag_embed(la).cfloat() @ matrix_exp(-tmp)
-    return exp_i(pauli_xs, th) @ torch.diag_embed(la).cfloat() @ exp_i(pauli_xs, -th)
+    # return matrix_exp(tmp) @ torch.diag_embed(la).cdouble() @ matrix_exp(-tmp)
+    return exp_i(pauli_xs, th) @ torch.diag_embed(la).cdouble() @ exp_i(pauli_xs, -th)
 
 
 def gen_rho_y_x(args):
@@ -324,7 +361,7 @@ def gen_rho_y_x(args):
     for alpha in alphas:
         distr_y = torch.distributions.dirichlet.Dirichlet(torch.ones(args.dim_Y, dtype=torch.float) * alpha)
         py = distr_y.sample()
-        rho_y.append(torch.diag(py).cfloat())
+        rho_y.append(torch.diag(py).cdouble())
     return torch.stack(rho_y).to(device)
 
 
@@ -332,7 +369,7 @@ def gen_rho_x(args):
     """ rho_x: [dim_X, dim_X, dim_X], dim_x stacks of {|x><x|} """
     rho_x = []
     for i in range(args.dim_X):
-        vec = torch.zeros(args.dim_X).cfloat()
+        vec = torch.zeros(args.dim_X).cdouble()
         vec[i] = 1
         rho_x.append(torch.outer(vec, vec))
     return torch.stack(rho_x).to(device)
@@ -341,10 +378,27 @@ def gen_rho_x(args):
 @torch.no_grad()
 def main(args):
     # pX = torch.ones(args.dim_X) / args.dim_X
-    distr_x = torch.distributions.dirichlet.Dirichlet(torch.ones(args.dim_X, dtype=torch.float) * 1000)
-    pX = distr_x.sample().to(device)
-    rho_X = gen_rho_x(args)
-    rho_Y_X = gen_rho_y_x(args).to(device)
+    if not os.path.exists('initial_points.pkl'):
+        distr_x = torch.distributions.dirichlet.Dirichlet(torch.ones(args.dim_X, dtype=torch.float) * 1000)
+        pX = distr_x.sample().to(device)
+        rho_X = gen_rho_x(args)
+        rho_Y_X = gen_rho_y_x(args).to(device)
+        init_sigma_T_X = initialize_sigma_T_X(args)
+        with open('initial_points.pkl', 'wb') as f:
+            pickle.dump(dict(
+                pX=pX,
+                rho_X=rho_X,
+                rho_Y_X=rho_Y_X,
+                init_sigma_T_X=init_sigma_T_X
+            ), f)
+    else:
+        with open('initial_points.pkl', 'rb') as f:
+            data_dict = pickle.load(f)
+        pX = data_dict['pX'].to(device)
+        rho_X = data_dict['rho_X'].to(device)
+        rho_Y_X = data_dict['rho_Y_X'].to(device)
+        init_sigma_T_X = data_dict['init_sigma_T_X'].to(device)
+
     assert is_positive(rho_Y_X), 'rho_Y|X is not positive.'
     assert is_hermitian(rho_Y_X), 'rho_Y|X is not hermitian.'
 
@@ -352,18 +406,25 @@ def main(args):
     y_coords = []
     betas = []
     iterations = []
-    for beta in tqdm(torch.arange(0.001, 30, 0.1)):
-        args.beta = beta
-        if args.algorithm == 'deterministic':
-            I_TX, I_YT, n = fixed_gamma_qib(args, pX, rho_Y_X, rho_X)
-        else:
-            I_TX, I_YT, n = adaptive_qib(args, pX, rho_Y_X, rho_X)
-        if I_TX is not None:
-            betas.append(beta)
-            iterations.append(n)
-            x_coords.append(I_TX.cpu())
-            y_coords.append(I_YT.cpu())
-    
+    # for beta in tqdm(torch.arange(0.001, 30, 0.1)):
+    #     args.beta = beta
+    #     if args.algorithm == 'deterministic':
+    #         I_TX, I_YT, n = fixed_gamma_qib(args, pX, rho_Y_X, rho_X, init_sigma_T_X)
+    #     else:
+    #         I_TX, I_YT, n = adaptive_qib(args, pX, rho_Y_X, rho_X, init_sigma_T_X)
+    #     if I_TX is not None:
+    #         betas.append(beta)
+    #         iterations.append(n)
+    #         x_coords.append(I_TX.cpu())
+    #         y_coords.append(I_YT.cpu())
+    args.beta = 6.
+    if args.algorithm == 'deterministic':
+        I_TX, I_YT, n = fixed_gamma_qib(args, pX, rho_Y_X, rho_X, init_sigma_T_X)
+    else:
+        I_TX, I_YT, n = fixed_gamma_qib(args, pX, rho_Y_X, rho_X, init_sigma_T_X)
+        I_TX, I_YT, n = adaptive_qib(args, pX, rho_Y_X, rho_X, init_sigma_T_X)
+    # print(I_TX - args.beta * I_YT)
+    assert False
     fig = plt.figure()
     plt.scatter(x_coords, y_coords)
     plt.xlabel('I(X;T)')
@@ -380,7 +441,7 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--algorithm', default='adaptive', type=str, help='deterministic or adaptive QIB.')
-    parser.add_argument('--gamma', default=0.8, type=float)
+    parser.add_argument('--gamma', default=1.6, type=float)
     parser.add_argument('--alpha', default=1.0, type=float)
     parser.add_argument('--beta', default=20., type=float)
     parser.add_argument('--dim_X', default=2**4, type=int, help='dimension of X')
@@ -393,8 +454,8 @@ if __name__ == '__main__':
     print(f'==== Running quantum algorithm on {device}....')
 
     # set_seed(0)
-    # args.writer = SummaryWriter(log_dir='./logs')
+    args.writer = SummaryWriter(log_dir=f'./logs3/gamma-{args.gamma}-quantum')
     main(args)
-    # args.writer.flush()
-    # args.writer.close()
+    args.writer.flush()
+    args.writer.close()
     